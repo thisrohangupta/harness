1afa5af393de8bff58db262b849017a8b84fcede
Rohan Gupta <52221549+thisrohangupta@users.noreply.github.com>
2023-03-02 09:01:10 -0800

    Create nginx.tf

---
+++ services/nginx.tf
@@ -0,0 +1,47 @@
+resource "harness_platform_service" "service" {
+  identifier  = "nginx" ## Service Identifier
+  name        = "nginx" ## Service Name to appear in Harness
+  description = "sample nginx app created via Harness terraform Provider"  
+  org_id      = "default" ## Replace with Harness Org Identifier for the resource
+  project_id  = "cdproduct" ## Replace with your Harness Project Identifier
+  yaml = <<-EOT
+                service:
+                  name: nginx ## Service Name (same as above)
+                  identifier: nginx ## Service Identifier, needs to be same as above
+                  serviceDefinition:
+                    spec:
+                      manifests:
+                        - manifest:
+                            identifier: manifest1
+                            type: K8sManifest
+                            spec:
+                              store:
+                                type: Github
+                                spec:
+                                  connectorRef: <+input> ## This is a connector in your account, project or Org to fetch source code
+                                  gitFetchType: Branch
+                                  paths:
+                                    - /deploy/
+                                  repoName: <+input> ## For an account level git connector, you can provide the Repo Name
+                                  branch: master
+                              skipResourceVersioning: false
+                      configFiles: ## This block is optional, this is for config files like a python script or json file you want to attach to the service
+                        - configFile:
+                            identifier: configFile1
+                            spec:
+                              store:
+                                type: Harness
+                                spec:
+                                  files:
+                                    - <+org.description>
+                      variables: ## These are service variables you can define
+                        - name: port
+                          type: String
+                          value: 8080
+                        - name: namespace
+                          type: String
+                          value: <+service.name>-<+env.name>
+                    type: Kubernetes
+                  gitOpsEnabled: false
+              EOT
+}
