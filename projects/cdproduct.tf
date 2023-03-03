e7a7a82239a9ca4bac3d92af85c99e0b24eadb40
Rohan Gupta <52221549+thisrohangupta@users.noreply.github.com>
2023-03-02 22:15:32 -0800

    Update cdproduct.tf

--- projects/cdproduct.tf
+++ projects/cdproduct.tf
@@ -1,6 +1,17 @@
+terraform {
+  required_providers {
+    harness = {
+      source = "harness/harness"
+      version = "0.14.10"
+    }
+  }
+}
+
+
 resource "harness_platform_project" "project" {
   identifier = "cdproduct"
   name       = "cdproduct"
   org_id     = "default"
   color      = "#0063F7"
 }
+
